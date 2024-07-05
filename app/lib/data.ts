abcd00df99b4b8cd798167400aecfc4b1aa1059e
dileepgg <dileep@onetikk.com>
2024-07-05 13:52:25 +0530

    updated code

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,8 +15,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+     console.log('Fetching revenue data...');
+     //await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -142,6 +142,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+ 
   try {
     const data = await sql<InvoiceForm>`
       SELECT
